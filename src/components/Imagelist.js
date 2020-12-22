a8e0d92520a8a4c2be0d15430480ab06c658972c
ravi rungta <4713rkr@gmail.com>
2020-12-22 14:03:47 +0530

    user input

--- src/components/Imagelist.js
+++ src/components/Imagelist.js
@@ -4,11 +4,9 @@ import './imageCard';
 import ImageCard from './imageCard';
 
 const Imagelist = (props)=>{
-    const a = [1,2 ,3 ,4,5]
     const imagesurls = props.images.map((image)=>{
         return <ImageCard  key={image.id} image={image}/>
     })
-    console.log(imagesurls )
     return (
         <div>
             images : {props.images.length}
